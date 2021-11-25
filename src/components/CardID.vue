17ccaa6fdb2b998666c56c37762a6c4706e42bfe
Vantol Bennett <vvantol2000@icloud.com>
2021-11-24 19:58:44 -0500

    plugin inital
    Initial commit for vue-me plugin

---
+++ src/components/CardID.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <div class="card card-profile mt-md-0 mt-5">
+          <div class="card-header mt-n4 mx-3 p-0 bg-transparent position-relative z-index-2">
+            <a class="d-block blur-shadow-image">
+              <img src="../../assets/img/examples/card-profile1.jpg" alt="img-blur-shadow" class="img-fluid border-radius-lg">
+            </a>
+          <div class="colored-shadow" style="background-image: url(&quot;../../assets/img/examples/card-profile1.jpg&quot;);"></div></div>
+          <div class="card-body text-center">
+            <h4 class="mb-0">Bruce Mars</h4>
+            <p>Atlanta, U.S.</p>
+            <div class="row justify-content-center text-center">
+              <div class="col-lg-4 col-4">
+                <h5 class="text-info mb-0">750</h5>
+                <small>Projects</small>
+              </div>
+              <div class="col-lg-4 col-4">
+                <h5 class="text-info mb-0">800+</h5>
+                <small>Hours</small>
+              </div>
+              <div class="col-lg-4 col-4">
+                <h5 class="text-info mb-0">24/7</h5>
+                <small>Support</small>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style>
+    .card {
+    box-shadow: 0 4px 6px -1px rgba(0,0,0,.1),0 2px 4px -1px rgba(0,0,0,.06);
+}
+.mt-md-0 {
+    margin-top: 0 !important;
+}
+.mt-5 {
+    margin-top: 3rem !important;
+}
+.card {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 0 solid rgba(0,0,0,.125);
+    border-radius: .75rem;
+}
+</style> 
